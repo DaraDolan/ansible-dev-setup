d77b011eb33672ad160e116d4de580a04f7db302
Dara <dara@Hades.>
2025-03-14 20:36:15 +0000

    Add zsh plugins and configuration

---
+++ roles/zsh/files/zinit.zsh
@@ -0,0 +1,11 @@
+source "$HOME/.local/share/zinit/zinit.git/zinit.zsh"
+
+zinit light zdharma-continuum/fast-syntax-highlighting
+zinit light zsh-users/zsh-autosuggestions
+zinit light zsh-users/zsh-completions
+zinit light zsh-users/zsh-history-substring-search
+zinit light jessarcher/zsh-artisan
+zinit light molovo/tipz
+zinit light romkatv/powerlevel10k
+
+zinit ice depth=1; zinit light romkatv/powerlevel10k
